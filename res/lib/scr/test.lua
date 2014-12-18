f687926f42ec4d28981eb0cff1ca876917e7ab22
Evan <iiechapman@gmail.com>
2014-12-17 19:14:45 -0500

    Adding ability to run app from script

---
+++ res/lib/scr/test.lua
@@ -0,0 +1,19 @@
+--Test lua
+
+--Globals
+
+appPath = "../../../SDLGameEngine.app"
+
+--Upon Load 
+os.execute("open " .. appPath)
+
+function greet()
+	-- Greets the user 
+	print("Hello lua")
+end
+
+
+function valueTest()
+	-- Printsa value sent from engine
+	print("Found testvalue: " .. valueFromEngine)
+end
