090d237fa901b5709d2a31274a0adc5000e80245
Evan Chapman <iiechapman@gmail.com>
2014-12-21 19:07:02 -0500

    Adding new tests

---
+++ res/lib/scr/test.lua
@@ -0,0 +1,13 @@
+--Test script to test sound
+print("Testing lua VM")
+
+
+function greet(...)
+	-- Greets engine
+	print("Hello Engine, I am Lua")
+end
+
+
+function valueTest()
+	print ("Value = " .. testValue)
+end
